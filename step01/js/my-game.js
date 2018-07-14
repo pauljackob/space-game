27f62472631a11cf8b9df13e3902375850a1fc9a
Paul Jackob <pauljackom911@hotmail.com>
2018-07-14 12:41:15 +0200

    added keys

--- step01/js/my-game.js
+++ step01/js/my-game.js
@@ -1,7 +1,14 @@
+const KEY_CODE_LEFT = 37;
+const KEY_CODE_RIGHT = 39;
+const KEY_CODE_SPACE = 32;
+
 const GAME_WIDTH = 800;
 const GAME_HEIGHT = 600;
 
 const GAME_STATE = {
+  leftPressed: false,
+  rightPressed: false,
+  spacePressed: false,
   playerX: 0,
   playerY: 0,
 };
@@ -20,22 +27,46 @@ function createPlayer(container) {
   setPosition(player, GAME_STATE.playerX, GAME_STATE.playerY);
 }
 
+function updatePlayer() {
+  if (GAME_STATE.leftPressed) {
+    GAME_STATE.playerX -= 5;
+  }
+  if (GAME_STATE.rightPressed) {
+    GAME_STATE.playerX += 5;
+  }
+}  
+
 function init() {
   const container = document.querySelector(".game");
   createPlayer(container);
 }
 
-function onKeyPress(e) {
-  if (e.key === 'a') {
-    GAME_STATE.playerX -= 5;
-    const player = document.querySelector(".player");
-    setPosition(player, GAME_STATE.playerX, GAME_STATE.playerY);
-  } else if (e.key === 'd') {
-    GAME_STATE.playerX += 5;
-    const player = document.querySelector(".player");
-    setPosition(player, GAME_STATE.playerX, GAME_STATE.playerY);
+function update(e) {
+  updatePlayer();
+  window.requestAnimationFrame(update);
+}
+
+function onKeyDown(e) {
+  if (e.keyCode === KEY_CODE_LEFT) {
+    GAME_STATE.leftPressed = true;
+  } else if (e.keyCode === KEY_CODE_RIGHT) {
+    GAME_STATE.rightPressed = true;
+  } else if (e.keyCode === KEY_CODE_SPACE) {
+    GAME_STATE.spacePressed = true;
+  }
+}
+
+function onKeyUp(e) {
+  if (e.keyCode === KEY_CODE_LEFT) {
+    GAME_STATE.leftPressed = false;
+  } else if (e.keyCode === KEY_CODE_RIGHT) {
+    GAME_STATE.rightPressed = false;
+  } else if (e.keyCode === KEY_CODE_SPACE) {
+    GAME_STATE.spacePressed = false;
   }
 }
 
 init();
-window.addEventListener("keypress", onKeyPress);
+window.addEventListener("keydown", onKeyDown);
+window.addEventListener("keyup", onKeyUp);
+window.requestAnimationFrame(update);
